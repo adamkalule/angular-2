97f9f1248cb833095fc0974df756020e70d8338f
adamkalule <adamkalule125@gmail.com>
2017-10-23 22:42:32 -0700

    angular 2 commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,18 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app';
+
+  items = ["Chips plain", "Chips Goats Meat", "Chips Chicken", "Pilao Plain", "Pilao and snacks"];
+
+  newItem = "";
+  pushItem = function() {
+    if(this.newItem != "") {
+      this.items.push(this.newItem);
+      this.newItem = "";
+    }
+  }
+
+  removeItem = function(index) {
+    this.items.splice(index, 1);
+  }
 }
